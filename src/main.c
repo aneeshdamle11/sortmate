d45bd623034ea5dd797c3e83ad22a1519face1a7
Aneesh Damle <aneesh.damle@gmail.com>
2024-11-26 14:03:25 +0530

    Added help display

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <getopt.h>
 #include "sort.h"
 
 void print_array(char *arr[], int n) {
@@ -7,11 +8,36 @@ void print_array(char *arr[], int n) {
     }
 }
 
+void display_help(void) {
+    printf("Usage: ./sortmate [OPTIONS]... [FILE]...\n");
+    printf("\nOptions:\n");
+    printf("  --help\t\tDisplay this help message and exit.\n");
+    printf("  -o, --output=FILE\tWrite result to specified file instead of standard output.\n");
+    return;
+}
+
 int main(int argc, char *argv[]) {
     // TODO: Update the usage after every option
-    if (argc > 1) {
-        printf("Usage: ./sortmate [OPTION] ... [FILE] ...\n");
+    if (argc > 2) {
+        display_help();
         return 0;
+    }
+
+    while (1) {
+        int c, option_index = 0;
+        static struct option long_options[] = {
+          {"help", no_argument, NULL, 0},
+          {NULL, 0, NULL, 0}
+        };
+
+        c = getopt_long(argc, argv, "", long_options, &option_index);
+        if (c == -1) break;
+
+        switch(c) {
+            case 0:
+                display_help();
+                return 0;
+        }
     }
 
     // take input
@@ -20,7 +46,7 @@ int main(int argc, char *argv[]) {
         "banana",
         "orange",
         "Grape",
-        "pomogranate",
+        "pineapple",
     };
 
     // sort
